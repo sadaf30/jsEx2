f2f02af379022f3ec3efc31f57719b6437a7cc04
ACE_E303 <ace_e303@e303-s12.lab.lagcc>
2017-06-09 16:44:49 -0400

    close button

--- js/mod1.js
+++ js/mod1.js
@@ -1,16 +1,41 @@
 // console.log('works');
 var sub=document.querySelector('#sub');
+var todo=document.querySelector('#list');
 function addToDo(event){
 	event.preventDefault();
 	// console.log('works');
 	var fname=document.querySelector('#fname');
-	var todo=document.querySelector('#list');
+	
 	if(fname.value===''){
 		return false;
 	}
-	todo.innerHTML+='<li>' + fname.value + '</li>';
+	todo.innerHTML='<li><i class="fa fa-window-close close-todo" aria-hidden="true"></i>'+ fname.value + '</li>' + todo.innerHTML;
 	fname.value='';
 	// console.log(fname.value)
 	// console.log(todo);
 }
+function removetodo(event){
+	// console.log(event.target);
+	// console.log(event.target.classList.contains('close-todo'));
+	if(event.target.classList.contains('close-todo')){
+		var li=event.target.parentElement;
+		todo.removeChild(li);
+
+	}
+}
+
 sub.addEventListener('click',addToDo,false);
+todo.addEventListener('click',removetodo,false);
+
+
+
+
+
+
+
+
+
+
+
+
+
